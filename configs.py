241a6c04012b6bb6f15d8633b1b7f3286232a62e
Chiyue Wei <1780277455@qq.com>
2024-06-03 17:23:48 -0400

    [minor] split networks file

---
+++ configs.py
@@ -0,0 +1,191 @@
+from collections import OrderedDict
+
+def spikeBERT_config(dataset):
+    dim = 768
+    batch_size = 1
+    time_steps = 4
+    depth = 12
+    num_head = 8
+    mlp_ratio = 4
+    if dataset == 'sst2':
+        sequence_length = 128
+    elif dataset == 'mr':
+        sequence_length = 256
+
+    spikeBERT_encoder = OrderedDict([
+        ('fc_q', [dim, dim * 3, sequence_length, batch_size, time_steps]),
+        ('lif_q', [dim * sequence_length * 3, batch_size, time_steps]),
+        ('attention', [dim, sequence_length, num_head, batch_size, time_steps]),
+        ('lif_attn', [dim * sequence_length, batch_size, time_steps]),
+        ('fc_o', [dim, dim, sequence_length, batch_size, time_steps]),
+        ('lif_o', [dim * sequence_length, batch_size, time_steps]),
+        ('fc_1', [dim, dim * mlp_ratio, sequence_length, batch_size, time_steps]),
+        ('lif_1', [dim * mlp_ratio * sequence_length, batch_size, time_steps]),
+        ('fc_2', [dim * mlp_ratio, dim, sequence_length, batch_size, time_steps]),
+        ('lif_2', [dim * sequence_length, batch_size, time_steps]),
+    ])
+
+    spikeBERT = OrderedDict()
+    for i in range(depth):
+        encoder_with_idx = OrderedDict([(key + '_enc_' + str(i), value) for key, value in spikeBERT_encoder.items()])
+        spikeBERT.update(encoder_with_idx)
+
+    return spikeBERT
+
+
+def spikformer_config(dataset='cifar10'):
+    if dataset == 'cifar10' or dataset == 'cifar100':
+        dim = 384
+        batch_size = 1
+        time_steps = 4
+        depth = 4
+        num_head = 12
+        mlp_ratio = 4
+        image_size = [32, 32, 16, 8]
+        sequence_length = image_size[-1] * image_size[-1]
+    elif dataset == 'cifar10dvs':
+        dim = 256
+        batch_size = 1
+        time_steps = 16
+        depth = 2
+        num_head = 16
+        mlp_ratio = 4
+        image_size = [64, 32, 16, 8]
+        sequence_length = image_size[-1] * image_size[-1]
+    else:
+        raise ValueError('Unknown dataset')
+
+    spikformer_SPS = OrderedDict([
+        ('conv2d_1', [image_size[0], dim // 8, dim // 4, 3, 1, 1, batch_size, time_steps]),
+        ('lif_1', [image_size[0] * image_size[0] * dim // 4, batch_size, time_steps]),
+        ('conv2d_2', [image_size[1], dim // 4, dim // 2, 3, 1, 1, batch_size, time_steps]),
+        ('lif_2', [image_size[1] * image_size[1] * dim // 2, batch_size, time_steps]),
+        ('maxpool2d_2', [image_size[1], dim // 2, 3, 1, 2, batch_size, time_steps]),
+        ('conv2d_3', [image_size[2], dim // 2, dim, 3, 1, 1, batch_size, time_steps]),
+        ('lif_3', [image_size[2] * image_size[2] * dim, batch_size, time_steps]),
+        ('maxpool2d_3', [image_size[2], dim, 3, 1, 2, batch_size, time_steps]),
+        ('conv2d_rpe', [image_size[3], dim, dim, 3, 1, 1, batch_size, time_steps]),
+        ('lif_rpe', [image_size[3] * image_size[3] * dim, batch_size, time_steps]),
+    ])
+    spikformer_encoder = OrderedDict([
+        ('fc_q', [dim, dim * 3, sequence_length, batch_size, time_steps]),
+        ('lif_q', [dim * sequence_length * 3, batch_size, time_steps]),
+        ('attention', [dim, sequence_length, num_head, batch_size, time_steps]),
+        ('lif_attn', [dim * sequence_length, batch_size, time_steps]),
+        ('fc_o', [dim, dim, sequence_length, batch_size, time_steps]),
+        ('lif_o', [dim * sequence_length, batch_size, time_steps]),
+        ('fc_1', [dim, dim * mlp_ratio, sequence_length, batch_size, time_steps]),
+        ('lif_1', [dim * mlp_ratio * sequence_length, batch_size, time_steps]),
+        ('fc_2', [dim * mlp_ratio, dim, sequence_length, batch_size, time_steps]),
+        ('lif_2', [dim * sequence_length, batch_size, time_steps]),
+    ])
+    spikformer = OrderedDict([(key + '_sps', value) for key, value in spikformer_SPS.items()])
+    for i in range(depth):
+        encoder_with_idx = OrderedDict([(key + '_enc_' + str(i), value) for key, value in spikformer_encoder.items()])
+        spikformer.update(encoder_with_idx)
+
+    return spikformer
+
+def SDT_config(dataset):
+    if dataset == 'cifar10' or dataset == 'cifar100':
+        dim = 512
+        batch_size = 1
+        time_steps = 4
+        depth = 4
+        num_head = 8
+        mlp_ratio = 4
+        image_size = [32, 32, 16, 8]
+        sequence_length = image_size[-1] * image_size[-1]
+    elif dataset == 'cifar10dvs':
+        dim = 512
+        batch_size = 1
+        time_steps = 10
+        depth = 2
+        num_head = 8
+        mlp_ratio = 4
+        image_size = [64, 32, 16, 8]
+        sequence_length = image_size[-1] * image_size[-1]
+
+        
+    SDT_sps = OrderedDict([('conv2d_1', [image_size[0], dim // 8, dim // 4, 3, 1, 1, batch_size, time_steps]),
+                           ('lif_1', [image_size[0] * image_size[0] * dim // 4, batch_size, time_steps]),
+                           ('maxpool2d_1', [image_size[0], dim // 4, 3, 1, 2, batch_size, time_steps]),
+                           ('conv2d_2', [image_size[1], dim // 4, dim // 2, 3, 1, 1, batch_size, time_steps]),
+                           ('lif_2', [image_size[1] * image_size[1] * dim // 2, batch_size, time_steps]),
+                            ('maxpool2d_2', [image_size[1], dim // 2, 3, 1, 2, batch_size, time_steps]),
+                            ('conv2d_3', [image_size[2], dim // 2, dim, 3, 1, 1, batch_size, time_steps]),
+                            ('lif_3', [image_size[2] * image_size[2] * dim, batch_size, time_steps]),
+                            ('maxpool2d_3', [image_size[2], dim, 3, 1, 2, batch_size, time_steps]),
+                            ('conv2d_rpe', [image_size[3], dim, dim, 3, 1, 1, batch_size, time_steps]),
+                            ('lif_sc', [image_size[3] * image_size[3] * dim, batch_size, time_steps])
+    ])
+    SDT_encoder = OrderedDict([('fc_q', [dim, dim * 3, sequence_length, batch_size, time_steps]),
+                               ('lif_q', [dim * sequence_length * 3, batch_size, time_steps]),
+                               ('attention', [dim, sequence_length, num_head, batch_size, time_steps]),
+                               ('fc_o', [dim, dim, sequence_length, batch_size, time_steps]),
+                               ('lif_o', [dim * sequence_length, batch_size, time_steps]),
+                               ('fc_1', [dim, dim * mlp_ratio, sequence_length, batch_size, time_steps]),
+                               ('lif_1', [dim * mlp_ratio * sequence_length, batch_size, time_steps]),
+                               ('fc_2', [dim * mlp_ratio, dim, sequence_length, batch_size, time_steps]),
+                               ('lif_2', [dim * sequence_length, batch_size, time_steps]),
+
+    ])
+    SDT = OrderedDict([(key + '_sps', value) for key, value in SDT_sps.items()])
+    for i in range(depth):
+        encoder_with_idx = OrderedDict([(key + '_enc_' + str(i), value) for key, value in SDT_encoder.items()])
+        SDT.update(encoder_with_idx)
+                        
+    return SDT
+
+
+def vgg16_config():
+    batch_size = 1
+    time_steps = 32
+    vgg16 = OrderedDict([
+        ('conv2d_1', [32, 64, 64, 3, 1, 1, batch_size, time_steps]),
+        ('lif_1', [32 * 32 * 64, batch_size, time_steps]),
+        ('maxpool2d_1', [32, 64, 2, 1, 2, batch_size, time_steps]),
+        ('conv2d_2', [16, 64, 128, 3, 1, 1, batch_size, time_steps]),
+        ('lif_2', [16 * 16 * 128, batch_size, time_steps]),
+        ('conv2d_3', [16, 128, 128, 3, 1, 1, batch_size, time_steps]),
+        ('lif_3', [16 * 16 * 128, batch_size, time_steps]),
+        ('maxpool2d_2', [16, 128, 2, 1, 2, batch_size, time_steps]),
+        ('conv2d_4', [8, 128, 256, 3, 1, 1, batch_size, time_steps]),
+        ('lif_4', [8 * 8 * 256, batch_size, time_steps]),
+        ('conv2d_5', [8, 256, 256, 3, 1, 1, batch_size, time_steps]),
+        ('lif_5', [8 * 8 * 256, batch_size, time_steps]),
+        ('conv2d_6', [8, 256, 256, 3, 1, 1, batch_size, time_steps]),
+        ('lif_6', [8 * 8 * 256, batch_size, time_steps]),
+        ('conv2d_7', [8, 256, 512, 3, 1, 1, batch_size, time_steps]),
+        ('lif_7', [8 * 8 * 512, batch_size, time_steps]),
+        ('conv2d_8', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
+        ('lif_8', [8 * 8 * 512, batch_size, time_steps]),
+        ('conv2d_9', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
+        ('lif_9', [8 * 8 * 512, batch_size, time_steps]),
+        ('conv2d_10', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
+        ('lif_10', [8 * 8 * 512, batch_size, time_steps]),
+        ('conv2d_11', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
+        ('lif_11', [8 * 8 * 512, batch_size, time_steps]),
+        ('conv2d_12', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
+        ('lif_12', [8 * 8 * 512, batch_size, time_steps]),
+        ('fc_0', [8 * 8 * 512, 4096, 1, batch_size, time_steps]),
+        ('lif_13', [4096, batch_size, time_steps]),
+        ('fc_1', [4096, 4096, 1, batch_size, time_steps]),
+        ('lif_14', [4096, batch_size, time_steps]),
+        # ('fc_2', [4096, 10, 1, batch_size, time_steps]),
+        # ('lif_15', [10, batch_size, time_steps]),
+    ])
+    return vgg16
+
+def lenet5_config():
+    batch_size = 1
+    time_steps = 32
+    lenet5 = OrderedDict([
+        ('conv2d_1', [16, 6, 16, 5, 0, 1, batch_size, time_steps]),
+        ('lif_2', [12 * 12 * 16, batch_size, time_steps]),
+        ('fc_0', [6 * 6 * 16, 120, 1, batch_size, time_steps]),
+        ('lif_3', [120, batch_size, time_steps]),
+        ('fc_1', [120, 84, 1, batch_size, time_steps]),
+        ('lif_4', [84, batch_size, time_steps]),
+    ])
+    return lenet5
--- networks.py
+++ networks.py
@@ -3,168 +3,7 @@ from collections import OrderedDict
 import pickle
 import torch
 from utils import img2col, get_density
-
-def spikformer_config(dataset='cifar10'):
-    if dataset == 'cifar10' or dataset == 'cifar100':
-        dim = 384
-        batch_size = 1
-        time_steps = 4
-        depth = 4
-        num_head = 12
-        mlp_ratio = 4
-        image_size = [32, 32, 16, 8]
-        sequence_length = image_size[-1] * image_size[-1]
-    elif dataset == 'cifar10dvs':
-        dim = 256
-        batch_size = 1
-        time_steps = 16
-        depth = 2
-        num_head = 16
-        mlp_ratio = 4
-        image_size = [64, 32, 16, 8]
-        sequence_length = image_size[-1] * image_size[-1]
-    else:
-        raise ValueError('Unknown dataset')
-
-    spikformer_SPS = OrderedDict([
-        ('conv2d_1', [image_size[0], dim // 8, dim // 4, 3, 1, 1, batch_size, time_steps]),
-        ('lif_1', [image_size[0] * image_size[0] * dim // 4, batch_size, time_steps]),
-        ('conv2d_2', [image_size[1], dim // 4, dim // 2, 3, 1, 1, batch_size, time_steps]),
-        ('lif_2', [image_size[1] * image_size[1] * dim // 2, batch_size, time_steps]),
-        ('maxpool2d_2', [image_size[1], dim // 2, 3, 1, 2, batch_size, time_steps]),
-        ('conv2d_3', [image_size[2], dim // 2, dim, 3, 1, 1, batch_size, time_steps]),
-        ('lif_3', [image_size[2] * image_size[2] * dim, batch_size, time_steps]),
-        ('maxpool2d_3', [image_size[2], dim, 3, 1, 2, batch_size, time_steps]),
-        ('conv2d_rpe', [image_size[3], dim, dim, 3, 1, 1, batch_size, time_steps]),
-        ('lif_rpe', [image_size[3] * image_size[3] * dim, batch_size, time_steps]),
-    ])
-    spikformer_encoder = OrderedDict([
-        ('fc_q', [dim, dim * 3, sequence_length, batch_size, time_steps]),
-        ('lif_q', [dim * sequence_length * 3, batch_size, time_steps]),
-        # qkv is fused
-        # ('fc_k', [dim, dim, 64, batch_size, time_steps]),
-        # ('lif_k', [dim * 64, batch_size, time_steps]),
-        # ('fc_v', [dim, dim, 64, batch_size, time_steps]),
-        # ('lif_v', [dim * 64, batch_size, time_steps]),
-        ('attention', [dim, sequence_length, num_head, batch_size, time_steps]),
-        ('lif_attn', [dim * sequence_length, batch_size, time_steps]),
-        ('fc_o', [dim, dim, sequence_length, batch_size, time_steps]),
-        ('lif_o', [dim * sequence_length, batch_size, time_steps]),
-        ('fc_1', [dim, dim * mlp_ratio, sequence_length, batch_size, time_steps]),
-        ('lif_1', [dim * mlp_ratio * sequence_length, batch_size, time_steps]),
-        ('fc_2', [dim * mlp_ratio, dim, sequence_length, batch_size, time_steps]),
-        ('lif_2', [dim * sequence_length, batch_size, time_steps]),
-    ])
-    spikformer = OrderedDict([(key + '_sps', value) for key, value in spikformer_SPS.items()])
-    for i in range(depth):
-        encoder_with_idx = OrderedDict([(key + '_enc_' + str(i), value) for key, value in spikformer_encoder.items()])
-        spikformer.update(encoder_with_idx)
-
-    return spikformer
-
-def SDT_config(dataset):
-    if dataset == 'cifar10' or dataset == 'cifar100':
-        dim = 512
-        batch_size = 1
-        time_steps = 4
-        depth = 4
-        num_head = 8
-        mlp_ratio = 4
-        image_size = [32, 32, 16, 8]
-        sequence_length = image_size[-1] * image_size[-1]
-    elif dataset == 'cifar10dvs':
-        dim = 512
-        batch_size = 1
-        time_steps = 10
-        depth = 2
-        num_head = 8
-        mlp_ratio = 4
-        image_size = [64, 32, 16, 8]
-        sequence_length = image_size[-1] * image_size[-1]
-
-        
-    SDT_sps = OrderedDict([('conv2d_1', [image_size[0], dim // 8, dim // 4, 3, 1, 1, batch_size, time_steps]),
-                           ('lif_1', [image_size[0] * image_size[0] * dim // 4, batch_size, time_steps]),
-                           ('maxpool2d_1', [image_size[0], dim // 4, 3, 1, 2, batch_size, time_steps]),
-                           ('conv2d_2', [image_size[1], dim // 4, dim // 2, 3, 1, 1, batch_size, time_steps]),
-                           ('lif_2', [image_size[1] * image_size[1] * dim // 2, batch_size, time_steps]),
-                            ('maxpool2d_2', [image_size[1], dim // 2, 3, 1, 2, batch_size, time_steps]),
-                            ('conv2d_3', [image_size[2], dim // 2, dim, 3, 1, 1, batch_size, time_steps]),
-                            ('lif_3', [image_size[2] * image_size[2] * dim, batch_size, time_steps]),
-                            ('maxpool2d_3', [image_size[2], dim, 3, 1, 2, batch_size, time_steps]),
-                            ('conv2d_rpe', [image_size[3], dim, dim, 3, 1, 1, batch_size, time_steps]),
-                            ('lif_sc', [image_size[3] * image_size[3] * dim, batch_size, time_steps])
-    ])
-    SDT_encoder = OrderedDict([('fc_q', [dim, dim * 3, sequence_length, batch_size, time_steps]),
-                               ('lif_q', [dim * sequence_length * 3, batch_size, time_steps]),
-                               ('attention', [dim, sequence_length, num_head, batch_size, time_steps]),
-                               ('fc_o', [dim, dim, sequence_length, batch_size, time_steps]),
-                               ('lif_o', [dim * sequence_length, batch_size, time_steps]),
-                               ('fc_1', [dim, dim * mlp_ratio, sequence_length, batch_size, time_steps]),
-                               ('lif_1', [dim * mlp_ratio * sequence_length, batch_size, time_steps]),
-                               ('fc_2', [dim * mlp_ratio, dim, sequence_length, batch_size, time_steps]),
-                               ('lif_2', [dim * sequence_length, batch_size, time_steps]),
-
-    ])
-    SDT = OrderedDict([(key + '_sps', value) for key, value in SDT_sps.items()])
-    for i in range(depth):
-        encoder_with_idx = OrderedDict([(key + '_enc_' + str(i), value) for key, value in SDT_encoder.items()])
-        SDT.update(encoder_with_idx)
-                        
-    return SDT
-
-
-def vgg16_config():
-    batch_size = 1
-    time_steps = 32
-    vgg16 = OrderedDict([
-        ('conv2d_1', [32, 64, 64, 3, 1, 1, batch_size, time_steps]),
-        ('lif_1', [32 * 32 * 64, batch_size, time_steps]),
-        ('maxpool2d_1', [32, 64, 2, 1, 2, batch_size, time_steps]),
-        ('conv2d_2', [16, 64, 128, 3, 1, 1, batch_size, time_steps]),
-        ('lif_2', [16 * 16 * 128, batch_size, time_steps]),
-        ('conv2d_3', [16, 128, 128, 3, 1, 1, batch_size, time_steps]),
-        ('lif_3', [16 * 16 * 128, batch_size, time_steps]),
-        ('maxpool2d_2', [16, 128, 2, 1, 2, batch_size, time_steps]),
-        ('conv2d_4', [8, 128, 256, 3, 1, 1, batch_size, time_steps]),
-        ('lif_4', [8 * 8 * 256, batch_size, time_steps]),
-        ('conv2d_5', [8, 256, 256, 3, 1, 1, batch_size, time_steps]),
-        ('lif_5', [8 * 8 * 256, batch_size, time_steps]),
-        ('conv2d_6', [8, 256, 256, 3, 1, 1, batch_size, time_steps]),
-        ('lif_6', [8 * 8 * 256, batch_size, time_steps]),
-        ('conv2d_7', [8, 256, 512, 3, 1, 1, batch_size, time_steps]),
-        ('lif_7', [8 * 8 * 512, batch_size, time_steps]),
-        ('conv2d_8', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
-        ('lif_8', [8 * 8 * 512, batch_size, time_steps]),
-        ('conv2d_9', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
-        ('lif_9', [8 * 8 * 512, batch_size, time_steps]),
-        ('conv2d_10', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
-        ('lif_10', [8 * 8 * 512, batch_size, time_steps]),
-        ('conv2d_11', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
-        ('lif_11', [8 * 8 * 512, batch_size, time_steps]),
-        ('conv2d_12', [8, 512, 512, 3, 1, 1, batch_size, time_steps]),
-        ('lif_12', [8 * 8 * 512, batch_size, time_steps]),
-        ('fc_0', [8 * 8 * 512, 4096, 1, batch_size, time_steps]),
-        ('lif_13', [4096, batch_size, time_steps]),
-        ('fc_1', [4096, 4096, 1, batch_size, time_steps]),
-        ('lif_14', [4096, batch_size, time_steps]),
-        # ('fc_2', [4096, 10, 1, batch_size, time_steps]),
-        # ('lif_15', [10, batch_size, time_steps]),
-    ])
-    return vgg16
-
-def lenet5_config():
-    batch_size = 1
-    time_steps = 32
-    lenet5 = OrderedDict([
-        ('conv2d_1', [16, 6, 16, 5, 0, 1, batch_size, time_steps]),
-        ('lif_2', [12 * 12 * 16, batch_size, time_steps]),
-        ('fc_0', [6 * 6 * 16, 120, 1, batch_size, time_steps]),
-        ('lif_3', [120, batch_size, time_steps]),
-        ('fc_1', [120, 84, 1, batch_size, time_steps]),
-        ('lif_4', [84, batch_size, time_steps]),
-    ])
-    return lenet5
+from configs import *
 
 def compute_num_OPS(nn):
     total_ops = 0
@@ -260,7 +99,7 @@ class Attention:
         self.act_q_tensor = Tensor([batch_size, time_steps, num_head, sequence_length, dim // num_head], 'spike', sparse=True)
         self.act_k_tensor = Tensor([batch_size, time_steps, num_head, sequence_length, dim // num_head], 'spike', sparse=True)
         self.act_v_tensor = Tensor([batch_size, time_steps, num_head, sequence_length, dim // num_head], 'spike', sparse=True)
-        if attention_type == 'spikformer':
+        if attention_type == 'spikformer' or attention_type == 'spikeBERT':
             self.attn_tensor = Tensor([batch_size, time_steps, num_head, sequence_length, sequence_length], 'fp8', sparse=True)
             self.output_tensor = Tensor([batch_size, time_steps, num_head, sequence_length, dim // num_head], 'fp8', sparse=False)
         elif attention_type == 'SDT':
@@ -289,6 +128,8 @@ def create_network(name, spike_info):
         config = vgg16_config()
     elif name == 'lenet5':
         config = lenet5_config()
+    elif name == 'spikeBERT':
+        config = spikeBERT_config(dataset=dataset)
     else:
         raise ValueError('Unknown network name')
     ops = []
@@ -341,7 +182,8 @@ def print_sparsity(network, spike_info):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    ops = create_network('SDT', 'sdt_cifar10dvs_new1.pkl')
+    # ops = create_network('spikeBERT', 'data/spikebert_sst2_new.pkl')
+    print_sparsity('spikeBERT', 'data/spikebert_sst2.pkl')
     # total_ops = compute_num_OPS(ops)
     # num_adder = 128
     # frequency = 500 * 1024 * 1024
