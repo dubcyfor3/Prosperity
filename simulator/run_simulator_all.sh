8c4561eada982dd07836f2714b81b6089e82ea10
Chiyue Wei <1780277455@qq.com>
2024-12-09 03:01:40 -0500

    [update] add script for main table

---
+++ simulator/run_simulator_all.sh
@@ -0,0 +1,8 @@
+python simulator.py --type Eyeriss
+python simulator.py --type Eyeriss_linear
+python simulator.py --type PTB
+python simulator.py --type SATO
+python simulator.py --type MINT
+python simulator.py --type Stellar
+python simulator.py --type A100
+python simulator.py --type Prosperity --use_cuda
